48826205fb27394b97ad8dba665936895d3ce529
Your Name <you@example.com>
2024-09-19 22:23:18 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,17 @@
+var a=2;
+
+let x=3;
+const y="script";
+console.log(x);
+console.log(y);
+            //block
+{
+    let b=4;
+    console.log(b);
+    console.log(a);
+}
+{
+    var p="hpp"
+    console.log(p);
+    
+}
